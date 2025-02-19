6ee17ecca2860e66a34d0d4a2f1d3ae7361d05c8
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-02-18 21:31:09 -0800

    plot all axis and combinations, export visualized data

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -55,3 +55,75 @@ participant_df.groupby(["participant"])["acc_y"].plot()
 ax.set_ylabel("acc_y")
 ax.set_xlabel("samples")
 plt.legend()
+
+
+# Plot multiple axis
+exercise = "squat"
+participant = "A"
+all_axis_df = df.query(f"exercise == '{exercise}'").query(f"participant == '{participant}'").reset_index()
+
+fig, ax = plt.subplots()
+all_axis_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax)
+ax.set_ylabel("acc_y")
+ax.set_xlabel("samples")
+plt.legend()
+
+
+# Create a loop to plot all combinations per sensor
+exercises = df["exercise"].unique()
+participants = df["participant"].unique()
+
+for exercise in exercises:
+    for participant in participants:
+        all_axis_df = (
+            df.query(f"exercise == '{exercise}'")
+            .query(f"participant == '{participant}'")
+            .reset_index()
+        )
+        
+        if len(all_axis_df) > 0:
+            fig, ax = plt.subplots()
+            all_axis_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax)
+            ax.set_ylabel("gyr_y")
+            ax.set_xlabel("samples")
+            plt.title(f"{exercise} ({participant})".title())
+            plt.legend()
+
+# Combine plots in one figure
+exercise = "row"
+participant = "A"
+combined_plot_df = (
+    df.query(f"exercise == '{exercise}'")
+    .query(f"participant == '{participant}'")
+    .reset_index(drop=True)
+)
+
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20,10))
+combined_plot_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax[0])
+combined_plot_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+
+ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+ax[1].set_xlabel("samples")
+
+
+# Loop over all combinations and export for both sensors
+for exercise in exercises:
+    for participant in participants:
+        combined_plot_df = (
+            df.query(f"exercise == '{exercise}'")
+            .query(f"participant == '{participant}'")
+            .reset_index()
+        )
+        
+        if len(combined_plot_df) > 0:
+            fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20,10))
+            combined_plot_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax[0])
+            combined_plot_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+
+            ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+            ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+            ax[1].set_xlabel("samples")
+            
+            plt.savefig(f"../../reports/figures/{exercise.title()} ({participant}).png")
+            plt.show()
